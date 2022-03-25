908890c46f06c775339b30e9d1163c409800f7fa
ayrton <ayrton.parkinson1@gmail.com>
2022-03-25 06:47:02 -0400

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 1..20 }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true
 end

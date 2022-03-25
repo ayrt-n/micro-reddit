ca1d4269003d1e4a515161bff5f6da97e8080464
ayrton <ayrton.parkinson1@gmail.com>
2022-03-25 07:46:12 -0400

    Add association with Comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  
+  has_many :comments
+
   validates :link, presence: true, length: { minimum: 3 }
   validates :body, presence: true, length: { maximum: 250 }
 end

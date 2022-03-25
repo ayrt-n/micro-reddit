8d7f57d8ec2c0d87e4dc6abbdb6f7942e5246bc0
ayrton <ayrton.parkinson1@gmail.com>
2022-03-25 07:11:40 -0400

    Add association with Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, uniqueness: true, length: { in: 1..20 }
   validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true
